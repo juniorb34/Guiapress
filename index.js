62df4addde8f12b245d0f0a6a86e68e3882586b1
juniorb34 <juniorb34fortal@gmail.com>
2020-10-29 12:43:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const Article = require('./articles/Article');
+const Category = require('./categories/Category');
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connection done!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use('/', categoriesController);
+app.use('/', articlesController);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(8080, () => {
+  console.log('The server is running!');
+});
